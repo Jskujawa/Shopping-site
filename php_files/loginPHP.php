16a4aec1ebc86c95b2a2383479106072f9a1c6d8
Jskujawa <48393020+Jskujawa@users.noreply.github.com>
2019-04-14 18:41:40 -0600

    cleaning up and commenting loginPHP

--- php_files/loginPHP.php
+++ php_files/loginPHP.php
@@ -6,34 +6,29 @@ require_once  'dblogin.php';
 $conn = new mysqli($hn, $un, $pw, $db);
 if($conn->connect_error) die($conn->connect_error);
 
-
-
+//pulling in username and pass from login
 $username = $_POST["username"];
 $password = $_POST["pwd"];
 
 	if (isset($_POST['btnsignup'])) {
+		//send to signup.php if signup button clicked
 		header("Location: ../signup.php");
 	}else{
-		//echo $username;
-		//echo "<br>";
-		//echo $password;
-		//echo "<br>";
-		
-		$query= "select password from account where username='$username' ";
+		$query= "select password from account where username='$username' ";//SQL query for password
 		$result = $conn->query($query);
 		$row = $result->fetch_array(MYSQLI_ASSOC);
 		
 		if ($password = $row['password']) {
-			session_start();//this must be called anywhere you want to use a session
-			$_SESSION['username'] = $user;	
-			$_SESSION['password'] = $tmp_password;
+			//set up username and password session variables and send to clothes_list
+			session_start();
+			$_SESSION['username'] = $username;	
+			$_SESSION['password'] = $password;
 			$_SESSION['loginMessage'] = "";
 			header("Location: ../clothes_list.php");
 		}
 		else {	
+			//send back error message if user/pass combo not found in DB
 			session_start();		
-			//echo $row['password'];
-			//echo "<br>Unsuccessful Login";
 			$_SESSION['loginMessage'] = "Invalid Username/Password";
 			header("Location: ../login.php");
 		}
