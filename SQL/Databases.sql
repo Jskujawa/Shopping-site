c73ddb8d4dc0d2f46cd6338892fab5663e74228f
Jskujawa <48393020+Jskujawa@users.noreply.github.com>
2019-04-21 13:54:24 -0600

    Adding in Amelias inserts
    Adding in some inserts and deleting old database.sql to remove confusion

--- SQL/Databases.sql
+++ SQL/Databases.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE IF NOT EXISTS `shoppingsite` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `shoppingsite`;
+USE `shoppingsite1`;
 
 
 create table campaign (
@@ -30,9 +30,6 @@ create table customer (
 )	
 ENGINE MyISAM;
 
-INSERT INTO `customer` (`customer_id`, `campaign_id`, `firstname`, `lastname`, `email`, `street`, `city`, `state`, `zipcode`, `phone`) VALUES
-('1234', '1', 'Walter', 'White', 'walterwhite@yahoo.com', '308 Negra Arroyo Lane', 'Albuquerque', 'NM', '87111', 2132445561),
-('2222', '1', 'Peter', 'Parker', 'spidey@gmail.com', '20 Ingram Street', 'Forest Hills', 'NY', '11375', 2132121222);
 
 
 
@@ -46,9 +43,7 @@ create table account (
 )	
 ENGINE MyISAM;
 
-INSERT INTO `account` (`username`, `password`, `role`, `customer_id`) VALUES 
-('Heisenberg', 'WaltJr', 'admin', '1234'),
-('SpiderMan', 'UncleBen', 'customer', '2222');
+
 
 
 
@@ -66,15 +61,13 @@ create table payment_method(
 )
 ENGINE MyISAM;
 
-INSERT INTO `payment_method` (`payment_id`, `customer_id`, `payment_type`, `firstname`, `lastname`, `card_number`, `expiration`, `security_code`) VALUES
-(1, 1234, 'visa', 'Walter', 'White', 1234123412341234, '08/22', 123),
-(2, 2222, 'mastercard', 'Peter', 'Parker', 8763926408215394, '09/24', 343);
+
 
 
 create table productorder (
 	 order_id int (20) NOT NULL AUTO_INCREMENT,
 	 customer_id int (100) NOT NULL,
-	 purchase_amount int (20) NOT NULL,
+	 purchase_amount FLOAT (20) NOT NULL,
 	 PRIMARY KEY (order_id),
 	 FOREIGN KEY (customer_id) REFERENCES customer(customer_id)	 
 )
@@ -103,13 +96,11 @@ ENGINE MyISAM;
 create table product (
 	product_id varchar (20) NOT NULL ,
 	product_name varchar (100) NOT NULL,
-	product_price char (20) NOT NULL,
+	product_price FLOAT (10) NOT NULL,
 	PRIMARY KEY (product_id)
 )
 
 ENGINE MyISAM;
-
-
 
 
 
@@ -175,9 +166,46 @@ create table inventoryreturn (
 	inventory_id varchar (100) NOT NULL,
 	order_id int (20) NOT NULL,
 	return_date char (10) NOT NULL,
-	return_amount varchar (100) NOT NULL,
+	return_amount FLOAT (20) NOT NULL,
 	quantity int (100) NOT NULL,
 	PRIMARY KEY (return_id, inventory_id),
 	FOREIGN KEY (order_id) REFERENCES productorder (order_id)
 )
 ENGINE MyISAM;
+
+
+
+
+
+INSERT INTO campaign (campaign_id, campaign_type, campaign_beg_date, campaign_end_date)
+VALUES ('4444','Spring Sale','2019-03-31','2019-04-30');
+INSERT INTO customer (customer_id, campaign_id, firstname, lastname, street, city, state, zipcode, email, phone)
+VALUES ('5555','4444','Walter','Ryan','70 N Street','SLC','UT','94103','walterryan@hotmails.com','8012788327');
+INSERT INTO `customer` (`customer_id`, `campaign_id`, `firstname`, `lastname`, `email`, `street`, `city`, `state`, `zipcode`, `phone`) VALUES
+('1234', '1', 'Walter', 'White', 'walterwhite@yahoo.com', '308 Negra Arroyo Lane', 'Albuquerque', 'NM', '87111', 2132445561),
+('2222', '1', 'Peter', 'Parker', 'spidey@gmail.com', '20 Ingram Street', 'Forest Hills', 'NY', '11375', 2132121222);
+INSERT INTO `account` (`username`, `password`, `role`, `customer_id`) VALUES 
+('Heisenberg', 'WaltJr', 'admin', '1234'),
+('SpiderMan', 'UncleBen', 'customer', '2222')
+('Ryan5','RyanRules','customer','5555');
+INSERT INTO payment_method (payment_id, customer_id, payment_type, firstname, lastname, card_number, expiration, security_code)
+VALUES ('6666','5555','Credit','Amelia','Hollberg','2554896750035640','03/24','687');
+INSERT INTO `payment_method` (`payment_id`, `customer_id`, `payment_type`, `firstname`, `lastname`, `card_number`, `expiration`, `security_code`) VALUES
+(1, 1234, 'visa', 'Walter', 'White', 1234123412341234, '08/22', 123),
+(2, 2222, 'mastercard', 'Peter', 'Parker', 8763926408215394, '09/24', 343);
+INSERT INTO productorder (order_id, customer_id, purchase_amount)
+VALUES ('89','5555','165.89');
+INSERT INTO shippingmethod (shipping_id, order_id, street, city, state, zipcode, departure_date, delivery_date)
+VALUES ('6338','89','80 H Street','SLC','UT','84103','2019-04-12','2019-04-17');
+INSERT INTO product (product_id, product_name, product_price)
+VALUES ('8989','Women White Sweater','35.99');
+INSERT INTO orderline (order_id, product_id, quantity)
+VALUES ('89','8989','1');	
+INSERT INTO inventory (inventory_id, product_id, quantity_on_hand)
+VALUES ('7777','8989','50');
+INSERT INTO supplier (supplier_id, supplier_name, street, city, state, zipcode, email, phone)
+VALUES ('2222','The Gap','122','LA','CA','54289','la@gap.com','2019998756');
+INSERT INTO inventoryorder (supplier_id, inventory_id, quantity, order_date, expected_arrival_date)
+VALUES ('2222','7777','60','2019-04-30','2019-05-07');
+INSERT INTO inventoryreturn (return_id, inventory_id, order_id, return_date, return_amount, quantity)
+VALUES ('3333','7777','89','2019-06-12','145.99','2');
