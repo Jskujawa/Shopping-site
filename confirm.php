eb0febbc4fbcd127a1e58b7723d092a7f6dfea51
xinyizhang1994 <48780207+xinyizhang1994@users.noreply.github.com>
2019-04-22 13:54:36 -0600

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -1,15 +1,43 @@
-<!DOCTYPE html>
+<?php
+session_start(); 
+
+if(!isset($_SESSION['username']))
+	{ $_SESSION['loginMessage'] = "Please Login First";
+		header("Location: login.php");}
+
+	$_SESSION['cart']="";
+
+?>
 <html>
 <head>
 	<meta charset="utf-8"> 
-	<title>confirm</title> 
+	<title>Clothes Lists</title> 
 	<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">  
 	<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
 	<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 <body>
-
-	
+	<div>
+		<span>
+				<span>
+					Current User:   <strong>
+						<?php 
+						IF(isset($_SESSION['username'])){
+						echo $_SESSION['username'];
+						}?></strong>
+					<a href = 'php_files/logout.php'> <input type="button" value="Logout"/></a>
+				</span>
+				<a href = 'account.php'> 
+					<input type="button" value="Your Account"/>
+				</a>
+				<a href = 'shoppingcart.php'> 
+					<input type="button" value="Shopping Cart"/>
+				</a>
+			</span>
+	</div>
+	<br>
+	<br>
+	<br>
 	<div class = "row">
 		<div class="col-sm-offset-1 col-sm-1">              
 			<img src="./picture/logo.png" alt="brand" width="100" height="100">
@@ -18,21 +46,15 @@
 			<h1>Congratulations!</h1>
 			<p>Your order has been successfully submitted.</p>
         <div>
-	<div
+	<div>
 
 <br>	
 <br>		
 <br>
-<div class="row">
-		<h4>Shipping information: </h4>
-		<img src="./picture/ship.jpg" class="img-response" alt="Airaisa" width="900" height="180"> 
-	</div>
-	
-	 
 		<div class="col-sm-offset-9 col-sm-8">
 			<button class="btn" type="submit" name="btnsubmit">Back</button>
-			</div>
- </div>
+		</div>
+	</div>
 
 
 
