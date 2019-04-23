77be51fc9382892cdf51101f7a45e54386cc6067
Jskujawa <48393020+Jskujawa@users.noreply.github.com>
2019-04-22 22:47:09 -0600

    Fixing header locations

--- php_files/emptyCart.php
+++ php_files/emptyCart.php
@@ -2,7 +2,7 @@
 session_start();
 if(!isset($_SESSION['username']))
 	{ $_SESSION['loginMessage'] = "Please Login First";
-		header("Location: login.php");
+		header("Location: ../login.php");
 		die();}
 
 IF (isset($_SESSION['cart'])){ $_SESSION['cart']= [];}
